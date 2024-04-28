00ff3f74749a698823043a882004452b39d9134c
hαrsh vαirαgi <harshb7839@gmail.com>
2024-04-28 20:49:34 +0530

    everforest has shifted from neanias to OurCodeBase...

--- plugin/packer_compiled.lua
+++ plugin/packer_compiled.lua
@@ -75,9 +75,10 @@ time([[try_loadstring definition]], false)
 time([[Defining packer_plugins]], true)
 _G.packer_plugins = {
   ["everforest-nvim"] = {
+    config = { "\27LJ\2\n‰\1\0\0\3\0\4\0\a6\0\0\0'\2\1\0B\0\2\0029\0\2\0005\2\3\0B\0\2\1K\0\1\0\1\0\4\25dim_inactive_windows\1\16ui_contrast\blow\23better_performance\2\15background\thard\nsetup\15everforest\frequire\0" },
     loaded = true,
     path = "/data/data/com.termux/files/home/.local/share/nvim/site/pack/packer/start/everforest-nvim",
-    url = "https://github.com/neanias/everforest-nvim"
+    url = "https://github.com/OurCodeBase/everforest-nvim"
   },
   ["packer.nvim"] = {
     loaded = true,
@@ -87,6 +88,10 @@ _G.packer_plugins = {
 }
 
 time([[Defining packer_plugins]], false)
+-- Config for: everforest-nvim
+time([[Config for everforest-nvim]], true)
+try_loadstring("\27LJ\2\n‰\1\0\0\3\0\4\0\a6\0\0\0'\2\1\0B\0\2\0029\0\2\0005\2\3\0B\0\2\1K\0\1\0\1\0\4\25dim_inactive_windows\1\16ui_contrast\blow\23better_performance\2\15background\thard\nsetup\15everforest\frequire\0", "config", "everforest-nvim")
+time([[Config for everforest-nvim]], false)
 
 _G._packer.inside_compile = false
 if _G._packer.needs_bufread == true then
