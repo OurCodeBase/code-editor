1abb4dc26714f3d49e17516c6da263d74b81daa0
hαrsh vαirαgi <harshb7839@gmail.com>
2024-05-10 01:50:39 +0530

    bug fixed: unable to use sync, update, clean options in lazy.nvim.
    In this bootstrap code `--depth=1` is the main reason to appear this error.

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -1,7 +1,7 @@
 -- check and install lazy if unavailable.
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  vim.fn.system({'git', 'clone', '--depth=1', '--filter=blob:none', 'https://github.com/folke/lazy.nvim.git', '--branch=stable', lazypath})
+  vim.fn.system({'git', 'clone', '--filter=blob:none', 'https://github.com/folke/lazy.nvim.git', '--branch=stable', lazypath})
 end
 vim.opt.rtp:prepend(lazypath)
 
